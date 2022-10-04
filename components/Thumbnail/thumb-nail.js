f2a79344d98086560a8055b27d66ef2446246440
msanmaz <47824518+msanmaz@users.noreply.github.com>
2022-10-04 13:37:34 +0100

    img fix

--- components/Thumbnail/thumb-nail.js
+++ components/Thumbnail/thumb-nail.js
@@ -9,12 +9,11 @@ const Thumbnail = ({
   images,
   bg,
   size = "small",
-  ratio='34'
 }) => {
   const initialImage = thumbnail || images
   return (
     <div
-      className={clsx(`relative aspect-[${ratio}/34] ${bg}`, {
+      className={clsx(`relative aspect-[34/34] ${bg}`, {
         "w-[80px]": size === "xs",
         "w-[180px]": size === "small",
         "w-[290px]": size === "medium",
@@ -27,9 +26,8 @@ const Thumbnail = ({
   )
 
 }
-const myLoader = ({ src, width, quality }) => {
-  return <div className="bg-gray-200"></div>
-}
+
+
 const ImageOrPlaceholder = ({
   images,
   size,
